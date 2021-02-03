3901dc1225c53f7ab78a5d309a90c2fe863c94ce
Lennart de Knikker <lennart@momkai.com>
2021-02-03 17:04:24 +0100

    remove circular dependency warnings

---
+++ src/routes/index.svelte
@@ -0,0 +1,111 @@
+<script>
+	import * as d3 from "d3";
+	import { onMount } from 'svelte'
+
+	onMount(() => {
+		let width = d3.select("#map").node().getBoundingClientRect().width
+		let height = d3.select("#map").node().getBoundingClientRect().height
+		let sensitivity = 75
+
+		let projection = d3.geoOrthographic()
+			.scale(250)
+			.center([0, 0])
+			.rotate([0,-30])
+			.translate([width / 2, height / 2])
+
+		const initialScale = projection.scale()
+		let path = d3.geoPath().projection(projection)
+
+		let svg = d3.select('#map')
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+
+		let globe = svg.append("circle")
+			.attr('class', 'globe')
+			.attr("fill", "#EEE")
+			.attr("stroke", "#000")
+			.attr("stroke-width", "0.2")
+			.attr("cx", width/2)
+			.attr("cy", height/2)
+			.attr("r", initialScale)
+
+		svg.call(d3.drag().on('drag', (event) => {
+			const rotate = projection.rotate()
+			const k = sensitivity / projection.scale()
+			projection.rotate([
+				rotate[0] + event.dx * k,
+				rotate[1] - event.dy * k
+			])
+			path = d3.geoPath().projection(projection)
+			svg.selectAll("path").attr("d", path)
+		}))
+			.call(d3.zoom().on('zoom', (event) => {
+			if(event.transform.k > 0.3) {
+				projection.scale(initialScale * event.transform.k)
+				path = d3.geoPath().projection(projection)
+				svg.selectAll("path").attr("d", path)
+				globe.attr("r", projection.scale())
+			}
+			else {
+				event.transform.k = 0.3
+			}
+		}))
+
+
+		const loadMap = async () => {
+			let map = svg.append("g")
+			const geoJson = await d3.json("https://raw.githubusercontent.com/michael-keith/mps_interests/master/view/js/charts/data/world_map.json")
+			map.append("g")
+					.attr("class", "countries" )
+					.selectAll("path")
+					.data(geoJson.features)
+					.enter().append("path")
+					.attr("class", "country")
+					.attr("d", path)
+					.attr("fill", "green")
+					.style('stroke', 'black')
+					.style('stroke-width', 0.3)
+					.style("opacity",0.8)
+		}
+
+		const rotateGlobe = () => {
+			d3.timer(function(elapsed) {
+				const rotate = projection.rotate()
+				const k = sensitivity / projection.scale()
+				projection.rotate([
+					rotate[0] - 1 * k,
+					rotate[1]
+				])
+				path = d3.geoPath().projection(projection)
+				svg.selectAll("path").attr("d", path)
+			},200)
+		}
+
+		const addLocations = () => {
+			let locations = svg.append("g")
+		}
+
+			loadMap()
+			rotateGlobe()
+			addLocations()
+	})
+</script>
+
+<style>
+	#map {
+  width: 100%;
+  height: 100vh;
+}
+
+.globe, .country {
+  cursor: grab;
+}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+
+<div id="map"></div>
